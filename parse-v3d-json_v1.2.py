bb05ca285645b3c00521199935c3379b636b8a8f
Ethan Brown <ethan-brown@hotmail.com>
2022-07-01 18:36:29 -0400

    Update parse-v3d-json_v1.2.py

--- parse-v3d-json_v1.2.py
+++ parse-v3d-json_v1.2.py
@@ -8,11 +8,11 @@ import json
 import csv
 
 
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import Tk 
 from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+Tk().withdraw() 
+filename = askopenfilename() 
 
 
 prevFile = ""
